34b5b13a288a7165e763ba85063c438f45dcedd5
branislavantic <banea@live.com>
2019-02-05 19:17:55 +0100

    initial state done

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { sSafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import Router from './src/Router';
+import reducers from './src/reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
 export default class App extends Component {
   render() {
